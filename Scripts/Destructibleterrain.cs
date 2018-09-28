5be034f4dcbeab9e6bc9c928012acfaf042ec466
lucasmuileboom <muileboomlucas@gmail.com>
2018-09-28 20:20:59 +0200

    scripts

---
+++ Scripts/Destructibleterrain.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructibleterrain : MonoBehaviour
+{
+    private Drawcircle drawcircle;
+    [SerializeField]private Texture2D texture;
+    [SerializeField]private GameObject map;
+    private GameObject newMap;
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        drawcircle = GetComponent<Drawcircle>();
+        newMap = Instantiate(map, new Vector3(0, 0, 0), Quaternion.Euler(0, 90, 0));
+        newMap.GetComponent<Renderer>().material.mainTexture = texture;
+
+        newMap.GetComponent<Renderer>().material.SetFloat("_Mode", 3f);
+        newMap.GetComponent<Renderer>().material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        newMap.GetComponent<Renderer>().material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+        newMap.GetComponent<Renderer>().material.renderQueue = 3000;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                if (hit.collider.tag == "ground")
+                {
+                    Vector2 pixelUV = hit.textureCoord;
+                    pixelUV.x *= newMap.GetComponent<Renderer>().material.mainTexture.width;
+                    pixelUV.y *= newMap.GetComponent<Renderer>().material.mainTexture.height;
+                    drawcircle.DrawCircle(texture, (int)pixelUV.x, (int)pixelUV.y, 20, Color.clear);
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/Drawcircle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawcircle : MonoBehaviour
+{
+    public void DrawCircle(Texture2D tex, int cx, int cy, int r, Color col)
+    {
+        int  px, nx, py, ny, d;
+
+        for (int x = 0; x <= r; x++)
+        {
+            d = (int)Mathf.Ceil(Mathf.Sqrt(r * r - x * x));
+            for (int y = 0; y <= d; y++)
+            {
+                px = cx + x;
+                nx = cx - x;
+                py = cy + y;
+                ny = cy - y;
+
+                tex.SetPixel(px, py, col);
+                tex.SetPixel(nx, py, col);
+                tex.SetPixel(px, ny, col);
+                tex.SetPixel(nx, ny, col);
+            }
+        }
+        tex.Apply();
+    }
+}
